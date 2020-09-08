8d78d30fa2e3769ad7006b99139bde0b68dd42c4
Napoleon Vuong <napolo101@gmail.com>
2020-09-08 15:46:34 -0700

    Removed extra dependancies and code in index.js and package.json

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const { prompt } = require("inquirer");
-const logo = require("asciiart-logo");
 const db = require("./db");
 require("console.table");
 
-init();
-
-// Display logo text, load main prompts
-function init() {
-    const logoText = logo({ name: "Employee Manager" }).render();
-
-    console.log(logoText);
-
-    loadMainPrompts();
-}
+loadMainPrompts();
 
 async function loadMainPrompts() {
     const { choice } = await prompt([
